967db6f01179ec23381488a6e261a540eaa132a2
medoz3ro <bsabo44@gmail.com>
2025-09-12 13:13:06 +0200

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Benjamin Sabo on 12.09.2025..
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsAppTests/NewsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppTests.swift
+//  NewsAppTests
+//
+//  Created by Benjamin Sabo on 12.09.2025..
+//
+
+import Testing
+@testable import NewsApp
+
+struct NewsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
